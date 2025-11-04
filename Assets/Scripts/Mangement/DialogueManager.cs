7ba7f202b66f7495d5f30d4407a860cabcc71608
Wix1410 <136693219+Wix1410@users.noreply.github.com>
2025-11-04 16:53:42 +0100

    add dialogue system

--- Assets/Scripts/Mangement/DialogueManager.cs
+++ Assets/Scripts/Mangement/DialogueManager.cs
@@ -1,16 +1,28 @@
+using TMPro;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    [Header("Bindings")]
     public GameObject dialogueUI;
     public GameObject pauseMenu;
+    public TMP_Text txt;
+
+    [Header("Arrays")]
+    string[] dialogue1;
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            dialogueUI.SetActive(false);
-            Time.timeScale = 1.0f;
+            for (int i = 0; i < dialogue1.Length; i++)
+            {
+                txt.text = dialogue1[i];
+                if (i == dialogue1.Length)
+                {
+                    dialogueUI.SetActive(true);
+                }
+            }
         }
     }
 }
